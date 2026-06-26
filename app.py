35cdcfc4898366afaaca0b1a8769d2fa1f49dc2e
Muhammad Ibrahim <mibrahim301105@gmail.com>
2026-06-26 10:02:53 +0500

    app.py

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ def load_data():
     return df
 
 df = load_data()
-df["date"] = pd.to_datetime(df["date"], format="mixed", dayfirst=False)
+df["date"] = pd.to_datetime(df["date"], dayfirst=True)
 
 def get_match_datetime(row):
     try:
